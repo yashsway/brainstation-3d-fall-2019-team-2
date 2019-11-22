a8dc6d307c81f4f1ba29f8e0a89de2495f5b200f
Luke Osgood <lukemosgood@gmail.com>
2019-11-22 13:39:45 -0500

    Header component started, app component

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import logo from '../../assets/logo.png';
+import './Header.scss';
+import { Link } from 'react-router-dom';
+
+
+class Header extends React.Component {
+
+  render() { 
+    return (
+      <div className="header">
+        <img src={logo} alt="" className="header__logo"/>
+        <div className="header__link1 header__link">Lifestyle</div>
+        <div className="header__link2 header__link">Social Media</div>
+        <div className="header__link3 header__link">Tech</div>
+        <div className="header__link4 header__link">bleepbloop</div>
+        <div className="header__link5 header__link">bleepbloop</div>
+        <div className="header__link6 header__link">bleepbloop</div>
+        <div className="header__link7 header__link">bleepbloop</div>
+      </div>
+    );
+  }
+}
+
+export default Header;
